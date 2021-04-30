b16a1a13b2b397bc058ef60ed8fa945dd741f1e9
Tuan <tuanphung2308@gmail.com>
2021-05-01 05:16:31 +0700

    - Labelling stuff

--- actions/check_bank_interest.py
+++ actions/check_bank_interest.py
@@ -9,17 +9,23 @@ class CheckBankInterest(Action):
         return "check_bank_interest"
 
     def run(self, dispatcher, tracker, domain):
-        bank_name = list(tracker.get_latest_entity_values('banks'))[0]
+        if tracker.get_latest_entity_values('banks') is not None:
+            bank_list = list(tracker.get_latest_entity_values('banks'))
+            if len(bank_list):
+                bank_name = list(tracker.get_latest_entity_values('banks'))[0]
+                print(bank_name)
 
-        with open('banking_interest.csv') as csv_file:
-            csv_reader = csv.reader(csv_file, delimiter=',')
-            for row in csv_reader:
-                if row[0] == bank_name or row[1] == bank_name:
-                    dispatcher.utter_message(
-                        text=f"Cảm ơn anh/chị đã quan tâm về lãi suất của ngân hàng {bank_name}. "
-                             f"Lãi suất tháng 4 của ngân hàng {bank_name} là {row[2]}%. "
-                             f"Tỉ suất cho vay của ngân hàng là {row[3]}%.")
-                    return []
+                with open('banking_interest.csv') as csv_file:
+                    csv_reader = csv.reader(csv_file, delimiter=',')
+                    for row in csv_reader:
+                        if row[0] == bank_name or row[1] == bank_name:
+                            dispatcher.utter_message(
+                                text=f"Cảm ơn anh/chị đã quan tâm về lãi suất của ngân hàng {bank_name}. "
+                                     f"Lãi suất tháng 4 của ngân hàng {bank_name} là {row[2]}%. "
+                                     f"Tỉ suất cho vay của ngân hàng là {row[3]}%.")
+                            dispatcher.utter_message(
+                                text=f"Em còn có thể giúp đỡ gì cho anh/chị không ạ?.")
+                            return []
         dispatcher.utter_message(
             text=f"Bọn em không tìm thấy thông tin về ngân hàng {bank_name}."
                  f"Tuy nhiên, để được hỗ trợ một cách nhanh nhất, anh chị có thể để lại thông tin để bọn em liên lạc hỗ trợ.")
--- actions/validate_custom_contact_form.py
+++ actions/validate_custom_contact_form.py
@@ -4,7 +4,9 @@ from typing import Text, List, Any, Dict, Optional
 from rasa_sdk import Tracker, FormValidationAction
 from rasa_sdk.executor import CollectingDispatcher
 from rasa_sdk.types import DomainDict
+
 phone_pattern = re.compile("^(0?)(3[2-9]|5[6|8|9]|7[0|6-9]|8[0-6|8|9]|9[0-4|6-9])[0-9]{7}$")
+
 
 class ValidateCustomContactForm(FormValidationAction):
     def name(self) -> Text:
